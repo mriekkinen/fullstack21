d37310737a197c969655246f4b811f8a1602e3e7
Matti Riekkinen <matti.riekkinen@icloud.com>
2021-04-20 13:53:16 +0300

    Tehtävät 2.19-2.20: puhelinluettelo

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import personService from './services/persons'
+import './index.css'
+
+const Notification = ({ message }) => {
+  if (!message.message) {
+    return null
+  }
+
+  return (
+    <div className={`notification ${message.type}`}>
+      {message.message}
+    </div>
+  )
+}
 
 const Filter = ({ filter, handleFilterChange }) => (
   <div>
@@ -55,6 +68,7 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [ message, setMessage ] = useState({})
 
   useEffect(() => {
     personService
@@ -63,6 +77,13 @@ const App = () => {
         setPersons(initialPersons)
       })
   }, [])
+
+  const showMessage = (message, type = 'success') => {
+    setMessage({ message, type })
+    setTimeout(() => {
+      setMessage({})
+    }, 5000);
+  }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -90,6 +111,7 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        showMessage(`Added ${returnedPerson.name}`)
       })
   }
 
@@ -101,6 +123,16 @@ const App = () => {
           setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
           setNewName('')
           setNewNumber('')
+          showMessage(
+            `Updated ${returnedPerson.name}`
+          )
+        })
+        .catch(error => {
+          showMessage(
+            `Information of ${changedPerson.name} has already been removed from server`,
+            'failure'
+          )
+          setPersons(persons.filter(person => person.id !== id))
         })
     }
   }
@@ -110,6 +142,14 @@ const App = () => {
       personService
         .remove(person.id)
         .then(() => {
+          setPersons(persons.filter(p => p.id !== person.id))
+          showMessage(`Deleted ${person.name}`)
+        })
+        .catch(error => {
+          showMessage(
+            `Information of ${person.name} has already been removed from server`,
+            'failure'
+          )
           setPersons(persons.filter(p => p.id !== person.id))
         })
     }
@@ -127,6 +167,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter
         filter={filter}
         handleFilterChange={handleFilterChange} />

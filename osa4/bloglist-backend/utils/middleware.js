7d6e7cb0a987f4c52e587ec80bd53d45b57eba6f
Matti Riekkinen <matti.riekkinen@icloud.com>
2021-04-28 17:12:24 +0300

    Tehtävät 4.1-4.2: blogilista

---
+++ osa4/bloglist-backend/utils/middleware.js
@@ -0,0 +1,40 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+/*
+const requestLogger = (req, res, next) => {
+  logger.info('Method:', req.method)
+  logger.info('Path:  ', req.path)
+  logger.info('Body:  ', req.body)
+  logger.info('---')
+  next()
+}
+*/
+
+morgan.token('body', req => JSON.stringify(req.body))
+
+const requestLogger = morgan(
+  ':method :url :status :response-time ms - :res[content-length] :body'
+)
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}

a95fd62ec6238b0c934681fc265e6ced3162d4e9
Matti Riekkinen <matti.riekkinen@icloud.com>
2021-06-08 08:59:45 +0300

    Exercises 9.4-9.5: Express and WebBMI

--- osa9/first-steps/bmiCalculator.ts
+++ osa9/first-steps/bmiCalculator.ts
@@ -1,17 +1,9 @@
+import { calculateBmi } from './bmi'
+
 interface BmiValues {
   height: number;
   weight: number;
 }
-
-type Category =
-  'Very severely underweight' |
-  'Severely underweight' |
-  'Underweight' |
-  'Normal (healthy weight)' |
-  'Overweight' |
-  'Obese Class I (Moderately obese)' |
-  'Obese Class II (Severely obese)' |
-  'Obese Class III (Very severely obese)';
 
 const parseArguments = (args: string[]): BmiValues => {
   if (args.length !== 4) {
@@ -27,28 +19,6 @@ const parseArguments = (args: string[]): BmiValues => {
 
   return {
     height, weight
-  }
-}
-
-const calculateBmi = (height: number, weight: number): Category => {
-  const bmi = weight / ((height / 100) ** 2);
-
-  if (bmi < 15) {
-    return 'Very severely underweight';
-  } else if (bmi < 16) {
-    return 'Severely underweight';
-  } else if (bmi < 18.5) {
-    return 'Underweight';
-  } else if (bmi < 25) {
-    return 'Normal (healthy weight)';
-  } else if (bmi < 30) {
-    return 'Overweight';
-  } else if (bmi < 35) {
-    return 'Obese Class I (Moderately obese)';
-  } else if (bmi < 40) {
-    return 'Obese Class II (Severely obese)';
-  } else {
-    return 'Obese Class III (Very severely obese)';
   }
 }
 
